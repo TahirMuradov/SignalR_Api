972583226ecf3a5793253e6e8d93835081d8aa1c
TahirMuradov <muradovtahir01@gmail.com>
2024-07-23 13:15:04 +0400

    Add project files.

---
+++ SignalR_Api/Hubs/MyHub.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR_Api.Hubs
+{
+    public class MyHub : Hub
+    {
+        public async Task SendMessage(string FromToConnectionId, string message, string? SendToconnectionId = null)
+        {
+            if (string.IsNullOrEmpty(SendToconnectionId))
+            {
+
+                await Clients.All.SendAsync("receiveMessage", FromToConnectionId, message);
+            }
+            else
+            {
+                string logMessage = await Clients.Client(SendToconnectionId).InvokeAsync<string>("receiveMessage", FromToConnectionId, message, SendToconnectionId, new());
+
+                Console.WriteLine(logMessage);
+
+            }
+
+        }
+    }
+}
